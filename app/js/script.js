c72213413194d711591689f6c63527e9d5e61bd1
Азамат Шарафутдинов <a.sharafutdinov@bftcom.com>
2022-10-13 00:04:30 +0300

    add

--- app/js/script.js
+++ app/js/script.js
@@ -46,5 +46,41 @@ $(document).ready(function () {
 
         return false;
     });
+
+    $(".js-open-menu").on("click" , function() {
+        $("body").toggleClass("active--menu")
+    })
 });
 
+
+$(document).mouseup(function(e) {
+
+    var mobileMenu = $("mobile-menu");
+    if (mobileMenu.has(e.target).length === 0) {
+       $("body").removeClass("active--menu")
+    }
+
+
+});
+
+$(function() {
+    document.getElementById('ajax-contact-form').addEventListener('submit', function(evt){
+      var http = new XMLHttpRequest(), f = this;
+      var th = $(this);
+      evt.preventDefault();
+      http.open("POST", "contact.php", true);
+      http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+          alert(http.responseText);
+          if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+            th.trigger("reset");
+          }
+        }
+      }
+      http.onerror = function() {
+        alert('Ошибка, попробуйте еще раз');
+      }
+      http.send(new FormData(f));
+    }, false);
+   
+  });
